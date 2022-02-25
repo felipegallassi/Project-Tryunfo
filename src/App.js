67fd23c0efc8e13e29639aa3e167fd0cfd5511c6
Felipe Gallassi <felipegallassi@gmail.com>
2022-02-25 20:26:54 -0300

    requisito 5 OK

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
 
+const patternState = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: '0',
+  cardAttr2: '0',
+  cardAttr3: '0',
+  cardImage: '',
+  cardRare: 'Normal',
+  cardTrunfo: false,
+  hasTrunfo: false,
+  isSaveButtonDisabled: true,
+};
+
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'Normal',
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      savedcards: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.isSaveButtonDisabled = this.isSaveButtonDisabled.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
 
   onInputChange({ target }) {
@@ -26,11 +43,66 @@ class App extends React.Component {
     this.setState((prevSt) => ({
       ...prevSt,
       [name]: value,
-    }));
+    }), () => this.isSaveButtonDisabled());
   }
 
-  onSaveButtonClick() {
-    return null;
+  onSaveButtonClick(event) {
+    event.preventDefault();
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      hasTrunfo,
+      savedCards,
+    } = this.state;
+    const card = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      hasTrunfo,
+    };
+    const cards = [...savedCards, card];
+    console.log(cards);
+    this.setState(patternState, () => {
+      this.setState({
+        savedCards: cards,
+      });
+    });
+  }
+
+  isSaveButtonDisabled() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    const attr1 = parseInt(cardAttr1, 10);
+    const attr2 = parseInt(cardAttr2, 10);
+    const attr3 = parseInt(cardAttr3, 10);
+    let disable = false;
+    const attrSum = attr1 + attr2 + attr3;
+    const maxAttrSum = 210;
+    const maxAttr = 90;
+    const minAttr = 0;
+    if (!cardName || !cardDescription || !cardImage || !cardRare) disable = true;
+    if (attrSum > maxAttrSum) disable = true;
+    if (attr1 > maxAttr || attr2 > maxAttr || attr3 > maxAttr) disable = true;
+    if (attr1 < minAttr || attr2 < minAttr || attr3 < minAttr) disable = true;
+    this.setState({
+      isSaveButtonDisabled: disable,
+    });
   }
 
   render() {
@@ -79,5 +151,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
